9e3b77a1cd4b8e354e4232e79477ed8437372cf0
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-15 13:23:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from pyscript import document
+
+print ("Hello world")
+output_div = document.querySelector("#textarea")
+
+output_div.innerText = "hello world!"
+output_div.innerText = "This text is taken from python"
